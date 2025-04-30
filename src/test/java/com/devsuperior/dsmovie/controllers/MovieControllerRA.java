e559937bc538cd2a1378c5c0517e360af53f271b
Lucas Muniz <lucasmunizfk@gmail.com>
2025-04-30 00:07:17 -0300

    test: findAll movies IT rest assured

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,16 +1,60 @@
 package com.devsuperior.dsmovie.controllers;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import io.restassured.http.ContentType;
 
 public class MovieControllerRA {
 	
-	@Test
-	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+	private String adminToken, clientToken;
+	private String movieTitle;
+	private Long existingId, nonExistingId;
+
+	@BeforeEach
+	public void setUp() {
+		baseURI = "http://localhost:8080"; 
+		movieTitle = "harry";
 	}
 	
 	@Test
-	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+		given()
+			.accept(ContentType.JSON)
+		.when()
+			.get("/movies")
+		.then()
+			.statusCode(200)
+			.body("content.findAll { it.score.toFloat() > 3 }.title", hasItems("The Witcher", "Venom: Tempo de Carnificina"))
+			.body("totalElements", is(29))
+			.body("content.id[5]", is(6))
+			.body("content.title[5]", equalTo("Django Livre"))
+			.body("content.score[5]", is(0.0F))
+			.body("content.count[5]", is(0))
+			.body("content.image[5]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/2oZklIzUbvZXXzIFzv7Hi68d6xf.jpg"));
+			
+	}
+	
+	@Test
+	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {	
+		given()
+			.accept(ContentType.JSON)
+		.when()
+			.get("/movies?title={title}", movieTitle)
+		.then()
+			.statusCode(200)
+			.body("totalElements", is(2))
+			.body("content.title", hasItems("Harry Potter e as Relíquias da Morte - Parte 1", "Harry Potter e a Pedra Filosofal"))
+			.body("content.id", hasItems(19,20))
+			.body("content.score[1]", is(0.0F))
+			.body("content.count[1]", is(0))
+			.body("content.image[1]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/lvOLivVeX3DVVcwfVkxKf0R22D8.jpg"));
+			
 	}
 	
 	@Test
